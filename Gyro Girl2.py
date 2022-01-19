9e1d0a12a606f0f7aa7220389626d89d6c82c314
Jan Tomsa <jan.tomsa.1976@gmail.com>
2022-01-19 23:29:40 +0100

    Tuned coefficient for my build, optimized imports + display

--- Gyro Girl2.py
+++ Gyro Girl2.py
@@ -1,16 +1,15 @@
-from mindstorms import MSHub, Motor, MotorPair, ColorSensor, DistanceSensor, App
-from mindstorms.control import wait_for_seconds, wait_until, Timer
-from mindstorms.operator import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, equal_to, not_equal_to
+from mindstorms import MSHub, Motor, MotorPair
 import math
-
 hub = MSHub()
 wheels = MotorPair('E','A')
 left_motor = Motor('A')
-
+# coefficients
 kp = 11 #proportional gain
 ki = 4.2 #integral gain
-kd = 92 #derivative gain
-roll_target = 87.358 #balance angle
+kd = 92 #derivative gain  ... increase if wobbling too much
+roll_target = 88.56 #balance angle  ... increase if drifting back (+0.01), decrease if drifting forward (-0.01)
+
+# working variables
 integral = 0
 error = 0
 start_target = 0
@@ -22,22 +21,8 @@ start = 0
 start_target = 0
 left_motor.set_degrees_counted(0)
 
-# Display a smiley
-hub.light_matrix.set_pixel(0,0) #left eye
-hub.light_matrix.set_pixel(0,1)
-hub.light_matrix.set_pixel(1,0)
-hub.light_matrix.set_pixel(1,1)
-
-hub.light_matrix.set_pixel(0,3) #right eye
-hub.light_matrix.set_pixel(0,4)
-hub.light_matrix.set_pixel(1,3)
-hub.light_matrix.set_pixel(1,4)
-
-hub.light_matrix.set_pixel(3,0) #mouth
-hub.light_matrix.set_pixel(3,4)
-hub.light_matrix.set_pixel(4,1)
-hub.light_matrix.set_pixel(4,2)
-hub.light_matrix.set_pixel(4,3)
+hub.light_matrix.set_orientation('right')
+hub.light_matrix.show('99099:99099:00000:90009:09990')  # Display a smiley face
 
 while hub.motion_sensor.get_roll_angle()<120 and hub.motion_sensor.get_roll_angle()>60:
     error = roll_target - hub.motion_sensor.get_roll_angle()
@@ -48,4 +33,4 @@ while hub.motion_sensor.get_roll_angle()<120 and hub.motion_sensor.get_roll_angl
     result = (error*kp) + (integral*ki) + (derivative*kd) + (start*ks)
     wheels.start_at_power(math.floor(result),0)
 wheels.stop()
-
+hub.light_matrix.show_image('ASLEEP')
