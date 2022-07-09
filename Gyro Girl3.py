2e052f2f2a52da963ee87d10ee35f5f67a50cb7c
jan-tomsa <jan.tomsa.1976@gmail.com>
2022-07-09 17:00:13 +0200

    Started experimenting with steering +Bit of refactoring

--- Gyro Girl3.py
+++ Gyro Girl3.py
@@ -4,6 +4,21 @@ import math
 import time
 from time import ticks_add, ticks_diff
 
+def hub_matrix_smiley(hub):
+    hub.light_matrix.show('99099:99099:00000:90009:09990') # Display a smiley face
+
+def hub_matrix_arrow_up(hub):
+    hub.light_matrix.show('00900:09090:90009:00900:00900') # Display up arrow
+
+def hub_matrix_arrow_down(hub):
+    hub.light_matrix.show('00900:00900:90009:09090:00900') # Display down arrow
+
+def hub_matrix_arrow_square(hub):
+    hub.light_matrix.show('99999:90009:90009:90009:99999') # Display square
+
+def hub_matrix_arrow_x(hub):
+    hub.light_matrix.show('90009:09090:00900:09090:90009') # Display x
+
 # Initialize color sensor
 cl_sens = ColorSensor('C')
 
@@ -18,9 +33,10 @@ kd = 92 #derivative gain  ... increase if wobbling too much
 #RT_STRAIGHT = 88.56 
 RT_STRAIGHT = 88.55 #balance angle... increase if drifting back (+0.01), decrease if drifting forward (-0.01)
 #RT_BACKWARD = 85
-RT_BACKWARD = 85.5
-RT_FORWARD = 90
+RT_BACKWARD = 86.5
+RT_FORWARD = 89.5
 roll_target = RT_STRAIGHT
+driving_angle = 0 # 0 = just go straight
 
 # working variables
 integral = 0
@@ -34,7 +50,7 @@ start = 0
 left_motor.set_degrees_counted(0)
 
 hub.light_matrix.set_orientation('right')
-hub.light_matrix.show('99099:99099:00000:90009:09990')  # Display a smiley face
+hub_matrix_smiley(hub)
 
 # Prepare for printing the inital roll angle
 roll_angle_printed = False
@@ -53,7 +69,8 @@ while (hub.motion_sensor.get_roll_angle()<120 and hub.motion_sensor.get_roll_ang
     deg_counted = left_motor.get_degrees_counted()
     start = (start_target - deg_counted)
     result = (error*kp) + (integral*ki) + (derivative*kd) + (start*ks)
-    wheels.start_at_power(math.floor(result),0)
+    
+    wheels.start_at_power(math.floor(result),driving_angle)
 
     # Print roll angle only once
     #if (not roll_angle_printed):
@@ -63,30 +80,30 @@ while (hub.motion_sensor.get_roll_angle()<120 and hub.motion_sensor.get_roll_ang
 
     if (counter == 400):
         roll_target = RT_BACKWARD
-        hub.light_matrix.show('00900:09090:90009:00900:00900')# Display up arrow
-
-    if (counter == 600):
-        roll_target = RT_STRAIGHT
-        hub.light_matrix.show('99999:90009:90009:90009:99999')# Display square
-
+        hub_matrix_arrow_up(hub)
 
     if (counter == 800):
-        roll_target = RT_FORWARD
-        hub.light_matrix.show('00900:00900:90009:09090:00900')# Display down arrow
+        roll_target = RT_STRAIGHT
+        hub_matrix_arrow_square(hub)
 
     if (counter == 1200):
-        roll_target = RT_STRAIGHT
-        hub.light_matrix.show('90009:09090:00900:09090:90009')# Display x
+        roll_target = RT_FORWARD
+        hub_matrix_arrow_down(hub)
 
+    if (counter == 1600):
+        roll_target = RT_STRAIGHT
+        hub_matrix_arrow_x(hub)
 
     # Process light sensor
     reflect = cl_sens.get_reflected_light()
-    if reflect > 60:
+    if reflect > 70:
         hub.status_light.on('blue')
+        driving_angle = 10
         #roll_target = RT_BACKWARD
-        #hub.light_matrix.show('00900:09090:90009:00900:00900')# Display up arrow
+        #hub_matrix_arrow_up(hub)
     else:
         hub.status_light.on('red')
+        driving_angle = 0
 
     counter = counter + 1
 
